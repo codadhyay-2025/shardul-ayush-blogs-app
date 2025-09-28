d0a9804ed4c2b57a6adcbf60dc36ae08323fd74d
ayush.kanase <kanaseayush4@gmail.com>
2025-09-28 20:22:43 +0530

    create new post screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom';
+
 import Dashboard from './Component/Dashboard/Dashboard.js';
 import Header from './Component/Header/Header.js';
+import CreateNewPost from './Component/CreatePost/CreateNewPost.js'
 function App() {
   return (
     <div>
-    <Dashboard/>
-    <Header/>
+      <Header/>
+    {/* <Dashboard/> */}
+    <CreateNewPost/>
+    
     </div>
   );
 }
