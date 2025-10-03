de9cb3f8b260ac99f42c2e1a4a9ec3040ffb22c5
ayush.kanase <kanaseayush4@gmail.com>
2025-10-03 16:14:58 +0530

    saved and delete changes done in blogs

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -1,10 +1,33 @@
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+
 import './Blogs.css';
 function Blogs() {
     const navigate = useNavigate();
     const navigateToCreatenewpost = () => {
         navigate('/createnewpost')
     }
+        const[blog,setBlog]=useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/blog`).then((res) => {
+            setBlog(res.data);
+
+        })
+    }, []);
+    const handelDelete=(id)=>{
+        axios.delete(`http://localhost:3001/blog/${id}`).then(()=>{
+            setBlog(blog.filter((b)=>b.id !==id));
+        });
+    }
+
+
+
+
+
+    
     return (
         <div className='backgroundcolr'>
             <div class>
@@ -19,25 +42,30 @@ function Blogs() {
                         <hr />
                     </div>
                     {/* main blogs start from here */}
+                    {blog.map((blog) => (
+                        console.log("title"+blog.Title),
+                        console.log(blog.Description),
+                        
 
-                    <div className='firstblog'>
-                        <div className='blogstitle'>Hello World</div>
-                        <div><strong>Created by:</strong><em>Shardul pawar</em></div>
-                        <div><strong>Created At:</strong><em>29/08/2005</em></div>
-                        <hr />
-                        <div> Hello Shardul Hello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHhardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello Shalo ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHellolo ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello lHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHellolo ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHellolo ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHellolo ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello  ShardulHello Shar my nam vvv vShardulHello Shar my namShardulHello Shar my namedulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHellolo ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHellolo ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello ShardulHello Shardul  ShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my namShardulHello Shar my nam</div>
-                        {/* last button section start from here */}
-                        <div className='lastbuttons'>
+                        <div className='firstblog' key={blog.id} >
+                            <div className='blogstitle'>{blog.Title}</div>
+                            <div ><strong>Created by:</strong><em>{blog.CreatedBy}</em></div>
+                            <div ><strong>Created At:</strong><em>{blog.CreatedAt}</em></div>
+                            <hr />
+                            <div >{blog.Description}</div>
+                            {/* last button section start from here */}
                             <div className='lastbuttons'>
-                                <button className='footerbuttons like'>like</button>
-                                <button className='footerbuttons dislike'>dislike</button>
-                            </div>
-                            <div className='lastbuttons'>
-                                <button className='footerbuttons edit'>edit</button>
-                                <button className='footerbuttons delete'>Delete</button>
+                                <div className='lastbuttons'>
+                                    <button className='footerbuttons like'><i class="fa fa-thumbs-up  LikeIcon" aria-hidden="true"></i>Like</button>
+                                    <button className='footerbuttons dislike'><i class="fa fa-thumbs-down  DislikeIcon" aria-hidden="true"></i>Dislike</button>
+                                </div>
+                                <div className='lastbuttons'>
+                                    <button className='footerbuttons edit'><i class="fa fa-pencil EditIcon" aria-hidden="true"></i>Edit</button>
+                                    <button className='footerbuttons delete' onClick={()=>handelDelete(blog.id)}><i class="fa fa-trash DeleteIcon" aria-hidden="true"></i>Delete</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    ))}
 
                 </div>
             </div>
